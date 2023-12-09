1c6e0291c63ec4db055065aef07b8b561e290fbf
surajkrgravitonweb <Suraj.kumar@gravitonweb.com>
2023-12-09 20:56:22 +0530

    first commit

---
+++ videoApi/urls.py
@@ -0,0 +1,12 @@
+# urls.py in YourAppName
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VideoViewSet
+
+router = DefaultRouter()
+router.register(r'videos', VideoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
